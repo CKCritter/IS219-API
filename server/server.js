218abffce119e677fa2bea5d1c207b70b8eafd74
CKCritter <CKCrider49@gmail.com>
2021-04-17 23:28:09 -0400

    CRUD Implemented

--- server/server.js
+++ server/server.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const server = express();
-const cors = require('cors')
+const cors = require('cors');
 const open = require("open");
-server.use(cors())
+const bodyParser = require('body-parser');
+server.use(cors());
 server.use(express.static('docs'));
 open("http://localhost:9080");
 
-server.get('/api/v1/hw_100', function(req, res) {
-        const mysql = require('mysql');
-        const conn = mysql.createConnection({
-            host: 'localhost',
-            port: '32000',
-            user: 'root',
-            password: 'root',
-            database: 'height_weight',
-        });
+const hwRoutes = require("./routes/hw.routes");
 
-        conn.connect();
-        conn.query('SELECT * FROM hw_100', function (err, rows, fields) {
-            if (err) throw err;
-            res.json({ "data": rows});
-        })
-        conn.end();
-    }
-)
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.use('/api/v1/hw_100', hwRoutes);
+
 
 server.set('port', process.env.PORT || 9080);
 server.set('ip', process.env.NODEJS_IP || '127.0.0.1');
