db5d7b3b60e77ae00fdb83e26105de36c6c8b35f
CKCritter <CKCrider49@gmail.com>
2021-04-30 01:08:57 -0400

    require auth changed to JWT

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -60,6 +60,25 @@ exports.login = function(req, res) {
     }
 }
 
+exports.requireAuth = function(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
 const crypto = require('crypto');
 
 const getHashedPassword = (password) => {
